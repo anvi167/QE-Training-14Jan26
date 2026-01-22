a6dfdf921e227e33f064fc1b90d88d2f5202c17c
anvi167 <Meka.Anvitha@ibm.com>
2026-01-22 16:42:45 +0530

    Add files via upload

---
+++ test/TestSuiteExampleWithSelectClasses.java
@@ -0,0 +1,12 @@
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
+import org.junit.platform.runner.JUnitPlatform;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses({
+    CalculatorTest.class, 
+    CSVFileParameterTest.class
+})
+public class TestSuiteExampleWithSelectClasses {}
+
