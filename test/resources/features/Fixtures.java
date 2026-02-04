da58b36254c209f670486f73dec2887a5fea037a
anvi167 <Meka.Anvitha@ibm.com>
2026-02-04 16:45:54 +0530

    Add files via upload

---
+++ test/resources/features/Fixtures.java
@@ -0,0 +1,18 @@
+package features;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Fixtures extends BaseClass {
+    @BeforeAll
+    public static void setUp() {
+        // Initialize Firefox Driver
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+    
+    @AfterAll
+    public static void tearDown() {
+        // Close the browser
+        driver.quit();
+    }
+}
